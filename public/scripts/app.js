4240c138b9bcaa66568716ab07032443e4efd743
unknown <gocreating@gmail.com>
2014-09-08 17:59:00 +0800

    lazy loading controller or other angular components and add contact page ui

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,4 @@
 'use strict';
-(function () {
 
 var app = angular.module('seed', [
 	'pascalprecht.translate',
@@ -9,6 +8,9 @@ var app = angular.module('seed', [
 	'myApp.directives',
 	'myApp.controllers'
 ]);
+
+(function () {
+
 
 /**************************************************************
  *                                                            *
@@ -62,12 +64,66 @@ app.run(['$rootScope', '$translatePartialLoader', '$translate', function ($rootS
 		// console.log(viewPath);
 		// var part = viewPath.substr(6, viewPath.length - 11);
 		// $translatePartialLoader.addPart(part);
-		loadTranslate($translatePartialLoader, toState.name);
+
+		var arr = toState.name.split('.');
+		arr.shift();
+		$translatePartialLoader.addPart(arr.join('/'));
+
+		// loadTranslate($translatePartialLoader, toState.name);
 	});
 
 	$rootScope.$on('$translatePartialLoaderStructureChanged', function () {
 		$translate.refresh();
 	});
+}]);
+
+app.config(['$controllerProvider', '$provide', '$compileProvider', function ($controllerProvider, $provide, $compileProvider) {
+	// Since the "shorthand" methods for component
+	// definitions are no longer valid, we can just
+	// override them to use the providers for post-
+	// bootstrap loading.
+	console.log( "Config method executed." );
+
+	// Let's keep the older references.
+	app._controller = app.controller;
+	app._service = app.service;
+	app._factory = app.factory;
+	app._value = app.value;
+	app._directive = app.directive;
+
+	// Provider-based controller.
+	app.controller = function( name, constructor ) {
+		$controllerProvider.register( name, constructor );
+		return( this );
+	};
+
+	// Provider-based service.
+	app.service = function( name, constructor ) {
+		$provide.service( name, constructor );
+		return( this );
+	};
+
+	// Provider-based factory.
+	app.factory = function( name, factory ) {
+		$provide.factory( name, factory );
+		return( this );
+	};
+
+	// Provider-based value.
+	app.value = function( name, value ) {
+		$provide.value( name, value );
+		return( this );
+	};
+
+	// Provider-based directive.
+	app.directive = function( name, factory ) {
+		$compileProvider.directive( name, factory );
+		return( this );
+	};
+
+	// NOTE: You can do the same thing with the "filter"
+	// and the "$filterProvider"; but, I don't really use
+	// custom filters.
 }]);
 
 /**************************************************************
@@ -119,7 +175,7 @@ app.config(['$stateProvider', '$urlRouterProvider', 'defaultStyle', function ($s
 			})
 			.state('root.general.contact', {
 				url: '/contact',
-				templateUrl: s('views/general/contact.html')
+				templateUrl: s('views/general/contact.html', 'bs')
 			})
 			.state('root.general.faq', {
 				url: '/faq',
